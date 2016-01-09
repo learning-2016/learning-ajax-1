05c65db7b25e73e51d249c92f16244e93964556d
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-01-09 10:37:56 +0800

    Added interval - when typing

--- foodstore.js
+++ foodstore.js
@@ -1,11 +1,14 @@
-function process() {
-  var food = encodeURIComponent(document.getElementById("userInput").value);
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById("underInput").innerHTML = xhttp.responseText; 
-    }
-  };
-  xhttp.open("GET", "foodstore.php?food=" + food, true);
-  xhttp.send();
+function process() { 
+	setInterval(function(){  
+		var food = encodeURIComponent(document.getElementById("userInput").value);
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+		  if (xhttp.readyState == 4 && xhttp.status == 200) {
+		    document.getElementById("underInput").innerHTML = xhttp.responseText; 
+		  }
+		};
+		xhttp.open("GET", "foodstore.php?food=" + food, true);
+		xhttp.send(); 
+	}, 2000);
+  
 }
