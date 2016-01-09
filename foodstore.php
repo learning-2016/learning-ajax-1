deaf5976f130e8923cc715f1ebb44d804fa6a25f
ransak123 <suarezmichaelandrew6@gmail.com>
2016-01-09 09:44:47 +0800

    initialize

---
+++ foodstore.php
@@ -0,0 +1,17 @@
+<?php
+header('Contend-Type: text/xml');
+echo '<?xml version ="1.0" encoding ="UTF-8" standalone = "yes"?>';
+
+echo '<response>';
+	$food = $_GET['food'];
+	$foodArray = array('tuna', 'bacon', 'beef', 'ham');
+	
+	if(in_array($food,$foodArray)){
+		echo 'We do have '.$food.'!';
+	}else if($food = ""){
+		echo 'Enter a food gwapo.';
+	}else{
+		echo 'Sorry gwapo we dont sell no '.$food.'!';
+	}
+echo '</response>';
+?>
